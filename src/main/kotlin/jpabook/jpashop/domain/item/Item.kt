3299a91c0e832d04c81dd4a7c3a9e9b7eada414b
sAmH365 <samh365@sAmH365ui-MacBookPro.local>
2023-03-06 01:56:17 +0900

    상품 엔티티 - 비즈니스 로직 추가

--- src/main/kotlin/jpabook/jpashop/domain/item/Item.kt
+++ src/main/kotlin/jpabook/jpashop/domain/item/Item.kt
@@ -3,6 +3,7 @@ package jpabook.jpashop.domain.item
 import jakarta.persistence.*
 import jpabook.jpashop.AllOpen
 import jpabook.jpashop.domain.Category
+import jpabook.jpashop.exception.NotEnoughStockException
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -21,4 +22,26 @@ abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     val categories: MutableList<Category> = mutableListOf()
+
+    // 비즈니스 로직
+
+    /**
+     * stock 증가
+     */
+    fun addStock(quantity: Int) {
+        this.stockQuantity = this.stockQuantity?.plus(quantity)
+    }
+
+    /**
+     * stock 감소
+     */
+    fun removeStock(quantity: Int) {
+        val restStock = this.stockQuantity?.minus(quantity)
+        if (restStock != null) {
+            if (restStock < 0) {
+                throw NotEnoughStockException("need more stock")
+            }
+        }
+        this.stockQuantity = restStock
+    }
 }
